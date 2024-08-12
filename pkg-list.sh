5177342512712957d0c41f1f8b235a3d7a8a6e06
mfwolffe <wolffemf@dukes.jmu.edu>
2024-08-12 18:10:52 -0400

    update package lists, helper + backup script

---
+++ pkg-list.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# this script is just to make @mfwolffe's life easier
+
+BKPTH=./pkglst-bk
+
+if [ ! -d $BKPTH ]; then
+  mkdir $BKPTH
+fi
+
+BKNM=packages-bk-$(date +'%m-%d:%T').txt
+
+cp releng/packages.x86_64 $BKPTH/$BKNM
+
+pacman -Qen | sed 's/ .*//' | cat - $BKPTH/$BKNM | sort --unique > releng/packages.x86_64
