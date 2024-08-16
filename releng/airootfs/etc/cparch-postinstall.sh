1c63493a8bd68cf1069414b8e22f374d6528a284
mfwolffe <wolffemf@dukes.jmu.edu>
2024-08-16 18:11:35 -0400

    need to reverse the signal handling and send a parent death signal

--- releng/airootfs/etc/cparch-postinstall.sh
+++ releng/airootfs/etc/cparch-postinstall.sh
@@ -4,24 +4,14 @@
 sed -i ‘s/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/g’ /etc/sudoers
 sed -i ‘s/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g’ /etc/sudoers
 
-# postgres
-# pgadmin
-# node
-# vscode
-# git things
+COL=$(tput cols)
+WLCM1="MUSIC CPARCH OPERATING SYSTEM POSTINSTALL SCRIPT"
+WLCM2="Matthew Forrester Wolffe"
+
+printf "%*s\n" $(((${#WLCM1}+$COL)/2)) "$WLCM1"
+printf "%*s\n\n\n" $(((${#WLCM2}+$COL)/2)) "$WLCM2"
 
 chsh -s /bin/zsh || eval "echo 'changing default shell failed' ; exit 1"
-
-#
-#                        █████    ██████ 
-#                       ░░███    ███░░███
-#   ██████    █████   ███████   ░███ ░░░ 
-#  ░░░░░███  ███░░   ███░░███  ███████   
-#   ███████ ░░█████ ░███ ░███ ░░░███░    
-#  ███░░███  ░░░░███░███ ░███   ░███     
-# ░░████████ ██████ ░░████████  █████    
-#  ░░░░░░░░ ░░░░░
-#
 
 printf "\nInstalling asdf:\n"
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0 || eval "echo 'Cloning asdf git repo failed' ; exit 1"
@@ -34,26 +24,27 @@ echo "sourcing ~/.zshrc"
 source ~/.zshrc
 
 asdf plugin add python
-asdf plugin add nodejs
 asdf install python 3.12.3
-asdf install nodejs 22.3.0
 asdf global python 3.12.3
+asdf plugin add nodejs
+asdf install nodejs 22.3.0
 asdf global nodejs 22.3.0
 
-#
-# POSTGRES
-#
-/bin/zsh ./etc/postinstall/postgres.sh
+konsole --hold -e "$(find . -name git-config.sh)" &
+wait
+konsole --hold -e "$(find . -name postgres.sh)" &
+wait
+konsole --hold -e "$(find . -name clone-repos.sh)" &
+wait
 
+konsole --hold -e "$(find . -name backend.sh)" &
+konsole --hold -e "$(find . -name frontend.sh)" &
 
-# TODO @mfwolffe:
-#                   * finish postinstall
-#                       * postgres
-#                       * forks/clones
-#                       * vscode???
-#                       * everything else???
-#                       *  
+firefox --new-tab "http://localhost:3000/"
+firefox --new-tab "http://localhost:8000"
 
+# kill -USR1
+# kill -USR2
 
 # cleanup
 systemctl disable cparch-postinstall.service
